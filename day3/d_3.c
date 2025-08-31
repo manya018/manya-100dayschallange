853d15b1f23e5a6ec03e5136b6dcb9abc48abac8
manya018 <manyagupta018@gmail.com>
2025-08-31 14:47:43 +0530

    Add files via upload

---
+++ day3/d_3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+
+    // Input values
+    printf("Enter first number: ");
+    scanf("%d", &a);
+    printf("Enter second number: ");
+    scanf("%d", &b);
+
+    // Swapping without third variable
+    a = a + b;
+    b = a - b;
+    a = a - b;
+
+    // Output swapped values
+    printf("\nAfter swapping:\n");
+    printf("First number = %d\n", a);
+    printf("Second number = %d\n", b);
+
+    return 0;
+}
