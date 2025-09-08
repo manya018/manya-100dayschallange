0035738612479deaf5e37bfa5babc099fea23802
manya018 <manyagupta018@gmail.com>
2025-09-08 23:52:30 +0530

    Add files via upload

---
+++ day18/d_18.c
@@ -0,0 +1,17 @@
+//Write a program to find the HCF (GCD) of two numbers.
+
+#include<stdio.h>
+
+int main(){
+    int a, b;
+printf("enter two numbers:");
+scanf("%d \n %d", &a, &b);
+while(a != b){
+    if(a>b)
+    a= a - b;
+    else
+    b= b - a; 
+}
+printf("HCF (GCD)= %d\n", a);
+    return 0;
+}
