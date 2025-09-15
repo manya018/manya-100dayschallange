23715c86e91f901f97d861bb20908010a3ccbabc
manya018 <manyagupta018@gmail.com>
2025-09-15 23:17:14 +0530

    Add files via upload

---
+++ day 22/d_22.c
@@ -0,0 +1,20 @@
+//Write a program to find the sum of the series: 1 + 3/4 + 5/6 + 7/8 + … up to n terms.
+
+
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    float sum = 0.0;
+
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        sum = sum + (float)(2*i - 1) / (2*i);  // add each term
+    }
+
+    printf("Sum of the series up to %d terms = %.4f\n", n, sum);
+
+    return 0;
+}
