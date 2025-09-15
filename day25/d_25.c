62eac7087ac94c86a2c030961e17f0db00ec13f1
manya018 <manyagupta018@gmail.com>
2025-09-15 23:54:55 +0530

    d_25.c

---
+++ day25/d_25.c
@@ -0,0 +1,25 @@
+/*Write a program to print the following pattern:
+*****
+****
+***
+**
+* */
+
+
+#include <stdio.h>
+
+int main() {
+    int i, j;
+
+    for (i = 5; i >= 1; i--) {        // rows
+        for (j = 1; j <= i; j++) {    // columns
+            printf("*");
+        }
+        printf("\n"); 
+    }
+
+    return 0;
+}
+
+
+
