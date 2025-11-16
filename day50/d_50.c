b2a4ebec3a95b7b90ea5c303ed1d9e618c3f5d39
manya018 <manyagupta018@gmail.com>
2025-11-16 15:00:52 +0530

    d_50.c

---
+++ day50/d_50.c
@@ -0,0 +1,28 @@
+//print all sub strings of a string
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+    int len, i, j, k;
+
+    printf("Enter a string: ");
+    scanf("%s", str); 
+
+    len = strlen(str);
+
+  
+    for (i = 0; i < len; i++) {
+    
+        for (j = i; j < len; j++) {
+           
+            for (k = i; k <= j; k++) {
+                printf("%c", str[k]);
+            }
+            printf("\n");
+        }
+    }
+
+    return 0;
+}
