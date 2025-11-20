777c7bb2dd89e5d9a53da080c0ba54e3b7444056
manya018 <manyagupta018@gmail.com>
2025-11-20 10:44:44 +0530

    d59.c

---
+++ day59/d59.c
@@ -0,0 +1,42 @@
+//Write a program to take an integer array arr and an integer k as inputs. Print the maximum sum of all the subarrays of size k.
+
+#include <stdio.h>
+
+int main() {
+    int n, k;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    scanf("%d", &k);
+
+    if (k > n) {
+        printf("Invalid");
+        return 0;
+    }
+
+    int windowSum = 0;
+
+    
+    for (int i = 0; i < k; i++) {
+        windowSum += arr[i];
+    }
+
+    int maxSum = windowSum;
+
+    
+    for (int i = k; i < n; i++) {
+        windowSum += arr[i];      
+        windowSum -= arr[i - k];   
+
+        if (windowSum > maxSum)
+            maxSum = windowSum;
+    }
+
+    printf("%d", maxSum);
+
+    return 0;
+}
