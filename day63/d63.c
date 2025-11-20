0f6e5188b0a0187d0b5260b5be7f98fa191e8695
manya018 <manyagupta018@gmail.com>
2025-11-20 10:50:01 +0530

    d63.c

---
+++ day63/d63.c
@@ -0,0 +1,33 @@
+//Write a program to take an integer array arr and an integer k as inputs. The task is to find the kth smallest element in the array. Print the kth smallest element as output.
+
+#include <stdio.h>
+
+int main() {
+    int n, k;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    scanf("%d", &k);
+
+
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[i]) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+
+    if (k > 0 && k <= n)
+        printf("%d", arr[k - 1]);
+    else
+        printf("Invalid");
+
+    return 0;
+}
