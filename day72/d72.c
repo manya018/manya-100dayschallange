88447e99255fc32c8c159190a840c226c14c6646
manya018 <manyagupta018@gmail.com>
2025-11-21 11:29:34 +0530

    d72.c

---
+++ day72/d72.c
@@ -0,0 +1,25 @@
+//Write a C program that opens an existing file (e.g., info.txt) and reads its contents using fgets(). The program should print all the lines to the console until EOF (end of file) is reached.
+
+#include <stdio.h>
+
+int main() {
+    FILE *fp;
+    char line[200];
+
+
+    fp = fopen("info.txt", "r");
+
+    if (fp == NULL) {
+        printf("Error: Could not open file info.txt\n");
+        return 1;
+    }
+
+    while (fgets(line, sizeof(line), fp) != NULL) {
+        printf("%s", line);
+    }
+
+    // Close the file
+    fclose(fp);
+
+    return 0;
+}
